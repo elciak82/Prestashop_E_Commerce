d66343a331043ed6d1cfaaf93ddd01717e19b08e
elciak82 <elciak82@gmail.com>
2023-05-03 13:25:56 +0200

    Add click methods to the footer component

--- src/main/java/webui/components/FooterComponent.java
+++ src/main/java/webui/components/FooterComponent.java
@@ -1,10 +1,11 @@
 package webui.components;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import webui.pages.BasePage;
+import webui.pages.*;
 
 public class FooterComponent extends BasePage {
 
@@ -67,7 +68,100 @@ public class FooterComponent extends BasePage {
         PageFactory.initElements(driver, this);
     }
 
+    @Step("Prices drop link click.")
+    public PricesDropPage clickOnPricesDropLink() {
+        priceDropLink.click();
+        return new PricesDropPage(driver);
+    }
 
+    @Step("New products link click.")
+    public NewProductsPage clickOnNewProductsLink() {
+        newProductsLink.click();
+        return new NewProductsPage(driver);
+    }
 
+    @Step("Best sales link click.")
+    public BestSellersPage clickOnBestSalesLink() {
+        bestSalesLink.click();
+        return new BestSellersPage(driver);
+    }
+
+    @Step("Delivery link click.")
+    public DeliveryPage clickOnDeliveryLink() {
+        deliveryLink.click();
+        return new DeliveryPage(driver);
+    }
+
+    @Step("Legal notice link click.")
+    public LegalNoticePage clickOnLegalNoticeLink() {
+        legalNoticeLink.click();
+        return new LegalNoticePage(driver);
+    }
+
+    @Step("Terms and conditions link click.")
+    public TermsAndConditionsPage clickOnTermsAndConditionsLink() {
+        termsAndCondLink.click();
+        return new TermsAndConditionsPage(driver);
+    }
+
+    @Step("About us link click.")
+    public AboutUsPage clickOnAboutUsLink() {
+        aboutUsLink.click();
+        return new AboutUsPage(driver);
+    }
+
+    @Step("Secure payment link click.")
+    public SecurePaymentPage clickOnSecurePaymentLink() {
+        securePaymentLink.click();
+        return new SecurePaymentPage(driver);
+    }
+
+    @Step("Contact us link click.")
+    public ContactUsPage clickOnContactUsLink() {
+        contactUsLink.click();
+        return new ContactUsPage(driver);
+    }
+
+    @Step("Site map link click.")
+    public SitemapPage clickOnSiteMapLink() {
+        sitemapLink.click();
+        return new SitemapPage(driver);
+    }
+
+    @Step("Stores link click.")
+    public StoresPage clickOnStoresLink() {
+        storesLink.click();
+        return new StoresPage(driver);
+    }
+
+    @Step("Personal info link click.")
+    public PersonalInfoPage clickOnPersonalInfoLink() {
+        personalInfoLink.click();
+        return new PersonalInfoPage(driver);
+    }
+
+    @Step("Orders link click.")
+    public OrdersPage clickOnOrdersLink() {
+        ordersLink.click();
+        return new OrdersPage(driver);
+    }
+
+    @Step("Credit slips link click.")
+    public CreditSlipsPage clickOnCreditSlipsLink() {
+        creditSlipsLink.click();
+        return new CreditSlipsPage(driver);
+    }
+
+    @Step("Addresses link click.")
+    public AddressesPage clickOnAddressesLink() {
+        addressesLink.click();
+        return new AddressesPage(driver);
+    }
+
+    @Step("Wishlist link click.")
+    public WishlistPage clickOnWishlistLink() {
+        wishListLink.click();
+        return new WishlistPage(driver);
+    }
 
 }
