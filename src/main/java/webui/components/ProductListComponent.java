59e142e1d4a73513ffffc7962792dfb3316622b4
elciak82 <elciak82@gmail.com>
2023-05-16 17:43:28 +0200

    Added ProductListComponent.java

---
+++ src/main/java/webui/components/ProductListComponent.java
@@ -0,0 +1,37 @@
+package webui.components;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import webui.pageobject.element.controls.Link;
+import webui.pages.BasePage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductListComponent extends BasePage {
+
+    private final List<Link> productMiniaturesList;
+
+    public ProductListComponent(WebDriver driver) {
+        super(driver);
+
+        productMiniaturesList = new ArrayList<>();
+        List<WebElement> productMiniatures = driver.findElements(By.cssSelector("[id='content'] article"));
+        for (WebElement e : productMiniatures) {
+            productMiniaturesList.add(new Link(e));
+        }
+    }
+
+    public List<Link> getMiniatures() {
+        return productMiniaturesList;
+    }
+
+    public ProductMiniatureComponent getProductMiniature() {
+        return new ProductMiniatureComponent(driver);
+    }
+
+
+
+
+}
