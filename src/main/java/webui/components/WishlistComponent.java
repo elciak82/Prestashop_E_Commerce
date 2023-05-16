299868e3da6e6953d35a1ceda7fdec02fb358c75
elciak82 <elciak82@gmail.com>
2023-05-16 22:20:14 +0200

    WishlistComponent - myWishlistButton added

--- src/main/java/webui/components/WishlistComponent.java
+++ src/main/java/webui/components/WishlistComponent.java
@@ -1,10 +1,21 @@
 package webui.components;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import webui.pageobject.element.controls.Button;
 import webui.pages.BasePage;
 
 public class WishlistComponent extends BasePage {
+    private final Button myWishlistButton;
+
     public WishlistComponent(WebDriver driver) {
         super(driver);
+
+        myWishlistButton = new Button(driver.findElement(By.cssSelector("[class='modal-content'] ul")));
+
+    }
+
+    public Button getMyWishlistButton(){
+        return myWishlistButton;
     }
 }
