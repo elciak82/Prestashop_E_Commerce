502437d5685af89784b7e9e2b1f4a81152c5f288
elciak82 <elciak82@gmail.com>
2023-05-16 18:49:48 +0200

    Added ProductMiniatureComponent in the ProductListComponent.java

--- src/main/java/webui/components/ProductMiniatureComponent.java
+++ src/main/java/webui/components/ProductMiniatureComponent.java
@@ -13,21 +13,39 @@ import java.util.List;
 public class ProductMiniatureComponent extends BasePage {
 
     private final Button addToWishlistButton;
+    private final Link quickViewLink;
+    private final List<Link> variantLinks;
 
     public ProductMiniatureComponent(WebDriver driver) {
         super(driver);
 
-
         addToWishlistButton = new Button(driver.findElement(By.className("wishlist-button-add")));
-    }
 
+        hoverOnElement(addToWishlistButton.getBaseElement());
+        quickViewLink = new Link(driver.findElement(By.xpath("//*[@id='content']/section/div/div[1]/article/div/div[1]/div/a")));
+
+        variantLinks = new ArrayList<>();
+        List<WebElement> variants = driver.findElements(By.cssSelector("[class = 'variant-links'] a"));
+        for (WebElement e : variants) {
+            variantLinks.add(new Link(e));
+        }
+
+    }
 
     public Button getAddToWishlistButton() {
         return addToWishlistButton;
     }
 
+    public Link getGetQuickViewLink() {
+        return quickViewLink;
+    }
+
     public WishlistComponent getWishlist() {
         return new WishlistComponent(driver);
+    }
+
+    public ProductQuickViewComponent getQuickView() {
+        return new ProductQuickViewComponent(driver);
     }
 
 }
