961b86c192a9e642952baeb510a2f566183bc602
elciak82 <elciak82@gmail.com>
2023-05-16 17:42:46 +0200

    Added ProductQuickViewComponent.java

---
+++ src/main/java/webui/components/ProductQuickViewComponent.java
@@ -0,0 +1,10 @@
+package webui.components;
+
+import org.openqa.selenium.WebDriver;
+import webui.pages.BasePage;
+
+public class ProductQuickViewComponent extends BasePage {
+    public ProductQuickViewComponent(WebDriver driver) {
+        super(driver);
+    }
+}
