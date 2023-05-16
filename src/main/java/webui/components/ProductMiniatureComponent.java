216c66c3424275210c87ed6f097e64a8acc43f89
elciak82 <elciak82@gmail.com>
2023-05-16 17:43:05 +0200

    Added ProductMiniatureComponent.java

--- src/main/java/webui/components/ProductMiniatureComponent.java
+++ src/main/java/webui/components/ProductMiniatureComponent.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import webui.pageobject.element.controls.Button;
+import webui.pageobject.element.controls.Link;
 import webui.pages.BasePage;
 
 import java.util.ArrayList;
@@ -11,27 +12,20 @@ import java.util.List;
 
 public class ProductMiniatureComponent extends BasePage {
 
-    private final List<Button> productMiniaturesList;
     private final Button addToWishlistButton;
 
     public ProductMiniatureComponent(WebDriver driver) {
         super(driver);
-        productMiniaturesList = new ArrayList<>();
-        List<WebElement> productMiniatures = driver.findElements(By.cssSelector("[id='content'] article"));
-        for (WebElement e : productMiniatures) {
-            productMiniaturesList.add(new Button(e));
-        }
+
 
         addToWishlistButton = new Button(driver.findElement(By.className("wishlist-button-add")));
     }
 
-    public List<Button> getMiniatures() {
-        return productMiniaturesList;
+
+    public Button getAddToWishlistButton() {
+        return addToWishlistButton;
     }
 
-    public void addToWishlist(WebDriver driver) {
-        addToWishlistButton.click();
-    }
     public WishlistComponent getWishlist() {
         return new WishlistComponent(driver);
     }
