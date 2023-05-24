8501851d9b44f681969cb104a393d23397b65310
elciak82 <elciak82@gmail.com>
2023-05-24 13:22:57 +0200

    Refactor: AccessoriesPage

--- src/main/java/webui/pages/AccessoriesPage.java
+++ src/main/java/webui/pages/AccessoriesPage.java
@@ -1,19 +1,19 @@
 package webui.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import webui.components.HeaderComponent;
+import webui.pageobject.element.controls.Label;
 
 public class AccessoriesPage extends HeaderComponent {
-
-    @FindBy (xpath = "//*[@id='wrapper']/div/nav/ol/li[4]/span")
-    private WebElement accessoryTitle;
+    Label accessoryTitle;
 
     public AccessoriesPage(WebDriver driver) {
         super(driver);
-        PageFactory.initElements(driver, this);
+        accessoryTitle = new Label(driver.findElement(By.xpath("//*[@id='wrapper']/div/nav/ol/li[4]/span")));
     }
 
     public String getAccessoryTitle(){
