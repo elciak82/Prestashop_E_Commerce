59f51b3ddc62229bc33cd4f0adf5f34aececa181
elciak82 <elciak82@gmail.com>
2023-05-24 13:26:43 +0200

    Refactor: AccountPage

--- src/main/java/webui/pages/AccountPage.java
+++ src/main/java/webui/pages/AccountPage.java
@@ -1,20 +1,20 @@
 package webui.pages;
 
 import io.qameta.allure.Step;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import webui.components.HeaderComponent;
+import webui.pageobject.element.controls.Link;
 
 public class AccountPage extends HeaderComponent {
-
-    @FindBy(id = "address-link")
-    private WebElement addressesLink;
+    Link addressesLink;
 
     public AccountPage(WebDriver driver) {
         super(driver);
-        PageFactory.initElements(driver, this);
+        addressesLink = new Link(driver.findElement(By.id("address-link")));
     }
 
     @Step("Click on the Addresses link.")
