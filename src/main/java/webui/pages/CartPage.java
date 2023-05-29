96591fb3b8cd447cf09ad96ec272f35e7e906720
elciak82 <elciak82@gmail.com>
2023-05-29 15:55:22 +0200

    CartPage added

---
+++ src/main/java/webui/pages/CartPage.java
@@ -0,0 +1,10 @@
+package webui.pages;
+
+import org.openqa.selenium.WebDriver;
+import webui.components.HeaderComponent;
+
+public class CartPage extends HeaderComponent {
+    public CartPage(WebDriver driver) {
+        super(driver);
+    }
+}
